d8f533f1e14fe7a4a3822b586ebe630c5597d9b3
GitRatton <maxime.albouy83@gmail.com>
2024-01-16 09:11:36 +0100

    Avancement Projet

--- exo1_starter_template.py
+++ exo1_starter_template.py
@@ -1,30 +1,59 @@
-class Robot():
-    __name = ""
-    __power = False
-    __current_speed = 0
-    __battery_level = 0
-    __states = ['shutown', 'running']
-    __if_charge = 0
+import time
 
-    def __init__(self, name):
+class Robot():   
+    
+
+   def __init__(self, name):
 	
       self.__name = name
       self.__current_speed = 0
       self.__battery_level = 0
+      self.__states = ['shutown', 'running']
       self.power = False
     
-    def allumer (self):
+   def allumer (self):
        self.power = self.__states[1]
        print("%s s'allume "%(self.__name))
       
-    def eteindre (self):
+   def eteindre (self):
        self.power = self.__states[0]
        print("%s s'éteint "%(self.__name))
-    def charge (self):
+
+   def charge (self):
        
       if self.__if_charge == 1 :
+          T = 0
           while (self.__battery_level <100):
-           for i in range (100):
+           for i in range (100) and T <=10:
               self.__battery_level += 1
+              time.sleep(1)
+              T += 1
+   
+   def decharge (self):
+       while (self.__battery_level >0):
+          for i in range(self.__battery_level):
+             self.__battery_level -= 1
+
+   @property
+   def power(self):
+       return self.__power
+
+   @power.setter
+   def power(self, power):
+       self.__power = power
+
+   @property
+   def action(self):
+      if self.__power:
+         return self.action
+
+
+if __name__ == '__main__':
+   r1 = Robot()
+   print(r1.power)
+
+
+
+   
 
          
