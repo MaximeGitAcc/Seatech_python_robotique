340f7162c6544466afeeeb603b0147988ac6ff5a
GitRatton <maxime.albouy83@gmail.com>
2024-01-16 11:25:45 +0100

    Last Change

--- exo1_play.py
+++ exo1_play.py
@@ -8,38 +8,59 @@ if CreationRobot == "Oui" or CreationRobot == "oui":
 else :
       print("Vous n'avez pas l'argent c'est ça ? Sortez tous de suite de notre magasin !")
 
-print("Voici les fonctionnalités dont je dispose :\n- M'allumer (allumer avec 'a')\n- M'éteindre (eteindre avec 'e')\n- Aller me mettre en charge (charge avec 'r')\n- Afficher mon pourcentage de Batterie en charge(Batterie avec 'b')\n- Avancer avec une certaine vitesse (Avancer avec 'z')\n- Reculer avec une certaine vitesse (Reculer avec 's')\n- S'arrêter net sur demande (Stop avec 'space')\n- Donner les caractéristiques du robot (Carac avec 'c')")
+print("Voici les fonctionnalités dont je dispose :\n- M'allumer (Allumer ou 'a')\n- M'éteindre (Eteindre ou 'e')\n- Aller me mettre en charge (Charge ou 'r')\n- Afficher mon pourcentage de Batterie en charge(Batterie ou 'b')\n- Avancer avec une certaine vitesse (Avancer ou 'z')\n- Reculer avec une certaine vitesse (Reculer ou 's')\n- S'arrêter net sur demande (Stop ou 'space')\n- Donner les caractéristiques du robot (Carac ou 'c')")
 
-def on_key_press_a(event):
-      print(f"You pressed {event.name}")
+Quefaire = input("Que voulez-vous que je fasses : ")
 
-def on_key_press_e(event):
-     print(f"You pressed {event.name}")
+if Quefaire == 'Allumer' or Quefaire == 'allumer':
+     r1.allumer()
+     r1.decharge()
+elif Quefaire == 'Eteindre' or Quefaire == 'eteindre':
+     r1.eteindre()
+elif Quefaire == 'Charge' or Quefaire == 'charge':
+     r1.charge()
+elif Quefaire == 'Batterie' or Quefaire == 'Batterie':
+     r1.AffichBatterie()
+elif Quefaire == 'Avancer' or Quefaire == 'avancer':
+     r1.Avancer()
+elif Quefaire == 'Reculer' or Quefaire == 'reculer':
+     r1.Reculer()
+elif Quefaire == 'Stop' or Quefaire == 'stop':
+     r1.Stop()
+elif Quefaire == 'Carac' or Quefaire == 'carac':
+     r1.Carac()
 
-def on_key_press_r(event):
-     print(f"You pressed {event.name}")
 
-def on_key_press_b(event):
-     print(f"You pressed {event.name}")
+# def on_key_press_a(event):
+#       print(f"You pressed {event.name}")
 
-def on_key_press_z(event):
-     print(f"You pressed {event.name}")
+# def on_key_press_e(event):
+#      print(f"You pressed {event.name}")
 
-def on_key_press_s(event):
-     print(f"You pressed {event.name}")
+# def on_key_press_r(event):
+#      print(f"You pressed {event.name}")
 
-def on_key_press_space(event):
-     print(f"You pressed {event.name}")
+# def on_key_press_b(event):
+#      print(f"You pressed {event.name}")
 
-def on_key_press_c(event):
-     print(f"You pressed {event.name}")
+# def on_key_press_z(event):
+#      print(f"You pressed {event.name}")
 
-keyboard.add_hotkey('a', on_key_press_a)
-keyboard.add_hotkey('e', on_key_press_e)
-keyboard.add_hotkey('r', on_key_press_r)
-keyboard.add_hotkey('b', on_key_press_b)
-keyboard.add_hotkey('z', on_key_press_z)
-keyboard.add_hotkey('s', on_key_press_s)
-keyboard.add_hotkey('space', on_key_press_space)
-keyboard.add_hotkey('c', on_key_press_c)
-keyboard.wait()
+# def on_key_press_s(event):
+#      print(f"You pressed {event.name}")
+
+# def on_key_press_space(event):
+#      print(f"You pressed {event.name}")
+
+# def on_key_press_c(event):
+#      print(f"You pressed {event.name}")
+
+# keyboard.add_hotkey('a', on_key_press_a)
+# keyboard.add_hotkey('e', on_key_press_e)
+# keyboard.add_hotkey('r', on_key_press_r)
+# keyboard.add_hotkey('b', on_key_press_b)
+# keyboard.add_hotkey('z', on_key_press_z)
+# keyboard.add_hotkey('s', on_key_press_s)
+# keyboard.add_hotkey('space', on_key_press_space)
+# keyboard.add_hotkey('c', on_key_press_c)
+# keyboard.wait()
--- exo1_starter_template.py
+++ exo1_starter_template.py
@@ -4,20 +4,26 @@ import math
 
 class Robot():     
 
+   __slots__ = ("__name","__current_speed","__battery_level","__states","__power","__vitesse","__direction","__EnCharge")
+
    def __init__(self):
 	
       self.__name = input("Le temps est venue de lui donner un petit nom : ")
       self.__current_speed = 0
-      self.__battery_level = 0
+      self.__battery_level = 20
       self.__states = ['shutown', 'running']
       self.power = False
       self.__vitesse = 0
       self.__direction = ['Avance','Recule']
+      self.__EnCharge = 0
       print("Bonjour, je suis votre nouveau robot. J'aime beaucoup le prénom que vous m'avez donnée :", self.__name)
     
    def allumer (self):
-       self.power = self.__states[1]
-       print("%s s'allume "%(self.__name))
+       if self.__battery_level >= 20:
+          self.power = self.__states[1]
+          print("%s s'allume "%(self.__name))
+       else :
+          print("Désolé le robot n'a pas assez de batterie.")
       
    def eteindre (self):
        self.power = self.__states[0]
@@ -25,31 +31,55 @@ class Robot():
 
    def charge (self):
        
-      if self.__if_charge == 1 :
+      if self.__battery_level <5:
+          print("Ma batterie est faible je vais me recharger !")
           T = 0
           while (self.__battery_level <100):
            for i in range (100) and T <=10:
+              self.__EnCharge = 1
               self.__battery_level += 1
               time.sleep(0.5)
               T += 1
    
    def decharge (self):
-       while (self.__battery_level >0):
+       while (self.__battery_level >5):
           for i in range(self.__battery_level):
+             self.__EnCharge = 0
              self.__battery_level -= 1
+             time.sleep(2)
 
    def Avancer (self):
-      self.__vitesse = input("Entrez une vitesse (Max 20 kmH ")
-      if self.__vitesse < 20:
-         self.__direction = self.__direction[0]
+      if self.power == 'running':
+         self.__vitesse = input("Entrez une vitesse (Max 20 kmH ")
+         if self.__vitesse < 20:
+            self.__direction = self.__direction[0]
+      else :
+         print("Le robot ne bouge pas, il ne semble pas être allumé")
 
    def Reculer (self):
-      self.__vitesse = abs(input("Entrez une vitesse (Max 5 kmH en arrière) "))
-      if -self.__vitesse > -5:
-         self.__direction = self.__direction[1]
+      if self.power == 'running':
+         self.__vitesse = abs(input("Entrez une vitesse (Max 5 kmH en arrière) "))
+         if -self.__vitesse > -5:
+            self.__direction = self.__direction[1]
+      else :
+         print("Le robot ne bouge pas, il ne semble pas être allumé")
     
+   def Carac (self):
+      if self.power == 'running':
+         Print("Voici mes caractéristiques :\n- Mon nom : ", self.__name, "\n- Mon deplacement : ", self.__direction , "\n- Vitesse :", self.__vitesse , "\n- En Charge : ", self.__EnCharge )
+      else :
+         print("Le robot ne bouge pas, il ne semble pas être allumé")
 
-      
+   def Stop (self):
+      if self.power == 'running':
+         self.__vitesse = self.__vitesse - self.__vitesse
+         print("je me suis arrêté, ma vitesse est donc à ", self.__vitesse)
+      else :
+         print("Le robot ne bouge pas, il ne semble pas être allumé")
+   
+   def AffichBatterie (self):
+      if self.__EnCharge == 1:
+         print("Mon niveau de batterie est de ", self.__battery_level, "%", end='\r', flush=True)
 
    @property
    def power(self):
