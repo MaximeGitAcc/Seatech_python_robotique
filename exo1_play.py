20563ab699474b76cd4d76b66c5b081a2324375c
GitRatton <maxime.albouy83@gmail.com>
2024-01-16 11:58:46 +0100

    Last change

--- exo1_play.py
+++ exo1_play.py
@@ -1,34 +1,36 @@
 import keyboard
+import time
 from exo1_starter_template import Robot
 
 CreationRobot = input("Bienvenue chez Aperture, voulez-vous créer un nouveau robot ? ")
 if CreationRobot == "Oui" or CreationRobot == "oui":
-      r1 = Robot()
+     r1 = Robot()
+     print("Voici les fonctionnalités dont je dispose :\n- M'allumer (Allumer ou 'a')\n- M'éteindre (Eteindre ou 'e')\n- Aller me mettre en charge (Charge ou 'r')\n- Afficher mon pourcentage de Batterie en charge(Batterie ou 'b')\n- Avancer avec une certaine vitesse (Avancer ou 'z')\n- Reculer avec une certaine vitesse (Reculer ou 's')\n- S'arrêter net sur demande (Stop ou 'space')\n- Donner les caractéristiques du robot (Carac ou 'c')")
 
+     while(True):
+          Quefaire = input("Que voulez-vous que je fasses : ")
+          if Quefaire == 'Allumer' or Quefaire == 'allumer':
+               r1.allumer()
+
+          elif Quefaire == 'Eteindre' or Quefaire == 'eteindre':
+                r1.eteindre()
+          elif Quefaire == 'Charge' or Quefaire == 'charge':
+               r1.charge()
+          elif Quefaire == 'Batterie' or Quefaire == 'Batterie':
+               r1.AffichBatterie()
+          elif Quefaire == 'Avancer' or Quefaire == 'avancer':
+               r1.Avancer()
+          elif Quefaire == 'Reculer' or Quefaire == 'reculer':
+               r1.Reculer()
+          elif Quefaire == 'Stop' or Quefaire == 'stop':
+               r1.Stop()
+          elif Quefaire == 'Carac' or Quefaire == 'carac':
+               r1.Carac()
+          Quefaire = ''
 else :
       print("Vous n'avez pas l'argent c'est ça ? Sortez tous de suite de notre magasin !")
 
-print("Voici les fonctionnalités dont je dispose :\n- M'allumer (Allumer ou 'a')\n- M'éteindre (Eteindre ou 'e')\n- Aller me mettre en charge (Charge ou 'r')\n- Afficher mon pourcentage de Batterie en charge(Batterie ou 'b')\n- Avancer avec une certaine vitesse (Avancer ou 'z')\n- Reculer avec une certaine vitesse (Reculer ou 's')\n- S'arrêter net sur demande (Stop ou 'space')\n- Donner les caractéristiques du robot (Carac ou 'c')")
 
-Quefaire = input("Que voulez-vous que je fasses : ")
-
-if Quefaire == 'Allumer' or Quefaire == 'allumer':
-     r1.allumer()
-     r1.decharge()
-elif Quefaire == 'Eteindre' or Quefaire == 'eteindre':
-     r1.eteindre()
-elif Quefaire == 'Charge' or Quefaire == 'charge':
-     r1.charge()
-elif Quefaire == 'Batterie' or Quefaire == 'Batterie':
-     r1.AffichBatterie()
-elif Quefaire == 'Avancer' or Quefaire == 'avancer':
-     r1.Avancer()
-elif Quefaire == 'Reculer' or Quefaire == 'reculer':
-     r1.Reculer()
-elif Quefaire == 'Stop' or Quefaire == 'stop':
-     r1.Stop()
-elif Quefaire == 'Carac' or Quefaire == 'carac':
-     r1.Carac()
 
 
 # def on_key_press_a(event):
--- exo1_starter_template.py
+++ exo1_starter_template.py
@@ -22,6 +22,15 @@ class Robot():
        if self.__battery_level >= 20:
           self.power = self.__states[1]
           print("%s s'allume "%(self.__name))
+
+
+         #  if (self.__battery_level >5):
+         #            for i in range(self.__battery_level):
+         #                 self.__EnCharge = 0
+         #                 self.__battery_level -= 1
+         #                 print("Mon niveau de batterie est de ", self.__battery_level, "%", end='\r', flush=True)
+         #                 time.sleep(0.5)
+                         
        else :
           print("Désolé le robot n'a pas assez de batterie.")
       
@@ -35,38 +44,36 @@ class Robot():
           print("Ma batterie est faible je vais me recharger !")
           T = 0
           while (self.__battery_level <100):
-           for i in range (100) and T <=10:
-              self.__EnCharge = 1
-              self.__battery_level += 1
-              time.sleep(0.5)
-              T += 1
+           for i in range (100):
+            if T <=10:
+               self.__EnCharge = 1
+               self.__battery_level += 1
+               time.sleep(0.5)
+               T += 1
+               print("Mon niveau de batterie est de ", self.__battery_level, "%", end='\r', flush=True)
+            
    
-   def decharge (self):
-       while (self.__battery_level >5):
-          for i in range(self.__battery_level):
-             self.__EnCharge = 0
-             self.__battery_level -= 1
-             time.sleep(2)
-
    def Avancer (self):
       if self.power == 'running':
-         self.__vitesse = input("Entrez une vitesse (Max 20 kmH ")
-         if self.__vitesse < 20:
+         self.__vitesse = input("Entrez une vitesse (Max 20 kmH) : ")
+         if self.__vitesse == '20':
             self.__direction = self.__direction[0]
+            print("Le robot se déplace à une vitesse de ", self.__vitesse, "vers l'avant")
       else :
          print("Le robot ne bouge pas, il ne semble pas être allumé")
 
    def Reculer (self):
       if self.power == 'running':
-         self.__vitesse = abs(input("Entrez une vitesse (Max 5 kmH en arrière) "))
-         if -self.__vitesse > -5:
+         self.__vitesse = input("Entrez une vitesse (Max 5 kmH en arrière) : ")
+         if self.__vitesse == '-5':
             self.__direction = self.__direction[1]
+            print("Le robot se déplace à une vitesse de ", self.__vitesse, "vers l'arrière")
       else :
          print("Le robot ne bouge pas, il ne semble pas être allumé")
     
    def Carac (self):
       if self.power == 'running':
-         Print("Voici mes caractéristiques :\n- Mon nom : ", self.__name, "\n- Mon deplacement : ", self.__direction , "\n- Vitesse :", self.__vitesse , "\n- En Charge : ", self.__EnCharge )
+         print("Voici mes caractéristiques :\n- Mon nom : ", self.__name, "\n- Mon deplacement : ", self.__direction , "\n- Vitesse :", self.__vitesse , "\n- En Charge : ", self.__EnCharge )
       else :
          print("Le robot ne bouge pas, il ne semble pas être allumé")
 
@@ -80,6 +87,8 @@ class Robot():
    def AffichBatterie (self):
       if self.__EnCharge == 1:
          print("Mon niveau de batterie est de ", self.__battery_level, "%", end='\r', flush=True)
+      else :
+         print("Le robot n'est pas en charge")
 
    @property
    def power(self):
